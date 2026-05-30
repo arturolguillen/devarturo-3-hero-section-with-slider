3ebd35ec6ac9c7471502395158b4523ce1a80255
Arturo Guillén <aguillen.js@gmail.com>
2026-05-30 18:19:05 -0400

    feat: integrate Tailwind CSS and remove Welcome component
    - Added Tailwind CSS and its Vite plugin to dependencies in package.json.
    - Removed the Welcome component and its associated styles from the project.
    - Updated Layout component to include global CSS and set a new title.
    - Modified index.astro to display a simple welcome message instead of the Welcome component.
    - Created a new global.css file to import Tailwind CSS and set custom font variables.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    fonts: [
+        {
+            name: 'Geist',
+            cssVariable: '--font-geist',
+            provider: fontProviders.google(),
+            styles: ['normal'],
+            weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        },
+    ],
+});
